10b5707456a167007f1e3b898138f54ab924cea7
DeividEDU <deividdvd@hotmail.com>
2022-01-05 17:33:27 -0300

    Check-in inicial do módulo Primeiros_passos

---
+++ Primeiros_passos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primeiros_passos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "Word!";
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
